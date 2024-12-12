363162cb89c4861e5ce395874c79160ca5d547f0
MartiVilas <marti.vilas14@gmail.com>
2024-12-12 09:32:19 +0100

    remove acidentally added files

--- main.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const button = document.getElementById("action-button");
-
-    button.addEventListener("click", () => {
-        alert("Botó clicat!");
-    });
-});
